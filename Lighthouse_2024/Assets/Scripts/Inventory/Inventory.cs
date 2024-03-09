7df82893d6e59af5122ef49da3d8d07da32ecbd5
Alex Quinton <alex.quinton77@gmail.com>
2024-03-09 11:47:35 -0700

    initial inventory commit, adds basic sprites & scripts

---
+++ Lighthouse_2024/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Singleton allows us to refer to this script very quickly
+    // by calling 'Inventory.instance'
+    #region Singleton
+
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
+    // delegate funcion that gets called any time inventory changes
+    // This is useful for managing the inventory UI
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 8;
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if (!item.isDefaultItem)
+        {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not enough space in inventory, max is " + space);
+                return false;
+            }
+
+                items.Add(item);
+            
+            if (onItemChangedCallback != null)
+                onItemChangedCallback.Invoke();
+        }
+
+        return true;
+    }
+
+    public void Remove(Item item)
+    { 
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+                onItemChangedCallback.Invoke();
+    }
+
+    // Start is called before the first frame update
+    private Canvas CanvasObject; // Assign in inspector
+    void Start()
+    {
+        //CanvasObject = GetComponent<Canvas>();
+        //CanvasObject.enabled = false;
+    }
+
+}
---
+++ Lighthouse_2024/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/*
+	Represents an item
+	I forgot its entire scope before i wrote this comment months later, woops
+*/
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    // https://youtu.be/HQNl3Ff2Lpo?list=PLPV2KyIb3jR4KLGCCAciWQ5qHudKtYeP7
+    new public string name = "New Item"; // new means overriding old definition that it has by default
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+
+	// string used to uniquely identify this item
+	// should always be in snake case
+	public string id;
+
+    public virtual void Use()
+    {
+        // Use the item
+        // Something might happen
+
+        Debug.Log("Using " + name);
+    }
+
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.Remove(this);
+    }
+}
